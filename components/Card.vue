40081b7a32fe96ab8679b0ba92a4e597bb6f486d
dk-raw <ddkatsios@protonmail.com>
2024-10-20 11:04:14 +0300

    styling

--- components/Card.vue
+++ components/Card.vue
@@ -3,7 +3,7 @@
     <div class="card bg-dark text-white card-img-bg">
       <img :src="image" class="card-img" :alt="description" />
       <div class="card-img-overlay">
-        <h4 class="card-title fs-lg-6 fs-4">{{ title }}</h4>
+        <h4 class="card-title fs-lg-6 fs-4 truncate-title">{{ title }}</h4>
         <p class="card-text">
           <span class="d-none d-lg-block">{{ description }}</span>
           <NuxtLink :to="`/articles/${slug}`" class="btn btn-outline-light mt-2 d-none d-lg-inline-block">
